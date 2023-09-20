e299f4488affce162e1a27e7a274bca3d3ecfd44
thuthu-c <thuannyba@gmail.com>
2023-09-20 09:13:28 -0300

    fix: put semaphore on prod

--- Concurrent Programming/src/ProdutorSem.java
+++ Concurrent Programming/src/ProdutorSem.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class ProdutorSem extends Thread{
 	Thread prod;
     ArrayList<Integer> produtos;
+    Semaphore semaforoprod; 
 
     public ProdutorSem(ArrayList<Integer>  produtos){
         prod = new Thread();
         this.produtos = produtos;
+        semaforoprod = new Semaphore(1,true);
 
     }
 
@@ -14,15 +17,18 @@ public class ProdutorSem extends Thread{
     @Override
     public void run(){
         for (int i = 0; i <60 ; i++) {
-            synchronized (produtos){
-                produtos.add(1);
-                System.out.println("Produzir:");
                 try {
+                    semaforoprod.acquire();
+                    produtos.add(1);
+                    System.out.println("Produzir:");
                     sleep(100);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-            }
+                finally{
+                    semaforoprod.release();
+                }
+            
         }
 
 
